1e3fff64a88705ce43fbc2dd0392aa68f98450a7
WatzingerM21052 <watzingerm21052@sus.htl-grieskirchen.at>
2026-03-18 08:55:41 +0100

    add admin page and routing; implement logout functionality in home component; add Swagger configuration files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Home } from './pages/home/home';
 import { authGuard } from './core/auth-guard';
+import { Admin } from './pages/admin/admin';
 
 export const routes: Routes = [
   { path: ``, redirectTo: `home`, pathMatch: `full` },
   { path: `home`, component: Home, canActivate: [authGuard] },
+  { path: `admin`, component: Admin, canActivate: [authGuard] },
   { path: '', loadComponent: () => import('./test-pages/test-general/test-general').then(x => x.TestGeneral) },
   { path: 'login', component: Login },
   { path: `**`, redirectTo: `home` }
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../services/auth-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './home.scss',
 })
 export class Home {
+  private authService = inject(AuthService);
 
+  logout(): void {
+    this.authService.logout();
+    window.location.reload();
+    // inject(Router).navigateByUrl(`home`);
+  }
 }
